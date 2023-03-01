a9392b7dd7c91587eebbc4f1b44f22c5c9c38735
mena0018 <rabie.menad@etudiant.univ-reims.r>
2023-03-01 14:57:45 +0100

    Installation et mise en place de react navigation

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React, { useMemo } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import useArticles from './src/hooks/useArticles';
 import Context from './src/context';
-import Provider from './src/router/Provider';
+import Title from './src/components/Title';
+import Footer from './src/components/Footer';
+import ArticleView from './src/router/routes/ArticleView';
+import CartView from './src/router/routes/CartView';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const { state, dispatch } = useArticles();
@@ -16,9 +23,18 @@ export default function App() {
 
   return (
     <Context.Provider value={value}>
-      <View style={styles.container}>
-        <Provider />
-      </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Title text="E-commerce" />
+
+          <Stack.Navigator initialRouteName="home">
+            <Stack.Screen name="home" component={ArticleView} />
+            <Stack.Screen name="cart" component={CartView} />
+          </Stack.Navigator>
+
+          <Footer />
+        </View>
+      </NavigationContainer>
     </Context.Provider>
   );
 }
